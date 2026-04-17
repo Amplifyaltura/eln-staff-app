e2180c0b3eb55c136f32801d1e810557705edcb0
Amplifyaltura <amplifyaltura@gmail.com>
2026-04-17 10:22:57 -0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+// supabase.js
+const supabaseUrl = 'https://gyijzqablwbkaryxdmvbs.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5aWp6cWFibHdia2FyeGRtdmJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MzY4MDIsImV4cCI6MjA5MTUxMjgwMn0.ThjI05W-koR0b6aQwh-iMFrjL2YD-1Wn-d8g4dWmpk8';
+
+const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
+
+export { supabase };
